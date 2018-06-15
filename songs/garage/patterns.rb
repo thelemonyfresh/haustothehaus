f37657b1cd5fdfd63b700db16fb8a5ec2b46ae2c
Daniel Porter <daniel.porter@scienceexchange.com>
2018-06-14 17:18:17 -0700

    Update sounds, cleanup viz

--- songs/garage/patterns.rb
+++ songs/garage/patterns.rb
@@ -38,14 +38,16 @@ end
 
 define :sub_bass do
   in_thread do
-    use_synth :beep
+    #use_synth :beep
+    use_synth :fm
     hd = 16
 
     s = play :C2, amp: 1, note_slide: 0.07,
-             attack: 0.01, decay: 2, sustain: 8, release: 6,
+             attack: 0.25, decay: 2, sustain: 8, release: 6,
              cutoff: 110, cutoff_slide: 8,
-             divisor: 2, depth: 0.6
-    #control s, cutoff: 80
+             divisor: 2,
+             depth: 0.6, depth_slide: 16
+    control s, depth: 1
     sleep 6
     control s, note: :F2
     sleep 2
@@ -55,43 +57,41 @@ define :sub_bass do
 
 end
 
-define :thorny_bass do
+define :thorny_bass_at do |amt|
   in_thread do
-    use_synth :dpulse
-    co = 100
+    use_synth :dpulse # work on this sound a bit -- should be more like the horns
+    co = 130
     pw = 0.3
 
     with_fx :lpf, cutoff: co do
-      use_transpose -12
+      use_transpose +12
 
-      nts = ring(:C3, :Cs3, :E3, :E3, :F3, :G3, :F3)
-      lns = ring(2,1.5,1,0.5,1,1,1)
-      hds = ring(2,2,0.5,0.75, 1, 1, 1)
+      num_notes = (amt*7).round
+      nts = ring(:C3, :Cs3, :E3, :E3, :F3, :G3, :F3).take(num_notes)
+      lns = ring(2,1.5,1,0.5,1,1,1).take(num_notes)
+      hds = ring(2,2,0.5,0.75, 1, 1, 1).take(num_notes)
       nts.each do |n|
         hd = hds.tick(:holds)
         horny_bass(n, hd)
         sleep lns.tick(:lens)
       end
-      nts1 = ring(:G3, :F3, :E3, :E3, :Cs3, :C3)
-      lns1 = ring(2,1.5,1,0.5,1,1)
-      hds1 = ring(2,2,0.5,0.75, 1, 1)
+      sleep (8 - lns.reduce(:+))
+
+      num_notes1 = (amt*6).round
+      nts1 = ring(:G3, :F3, :E3, :E3, :Cs3, :C3).take(num_notes1)
+      lns1 = ring(2,1.5,1,0.5,1,1).take(num_notes1)
+      hds1 = ring(2,2,0.5,0.75, 1, 1).take(num_notes1)
 
       nts1.each do |n|
         hd = hds1.tick(:holds1)
-        puts n
-
-        if true #[:E3].include?(n)
-          s = play n, attack: 0.05, attack_level: 1, decay: 0.2*hd, sustain: 0.25*hd, sustain_level: 0.75, release: 0.3*hd,
-                   pulse_width: pw,
-                   cutoff: 115, cutoff_slide: 2
-          control s, cutoff: 95
-        end
-
+        horny_bass(n, hd)
         sleep lns1.tick(:lens1)
       end
-
-      sleep 1
+      sleep (8 - lns1.reduce(:+))
     end
   end
+end
 
+define :thorny_bass do
+  thorny_bass_at(1)
 end
--- songs/garage/sounds.rb
+++ songs/garage/sounds.rb
@@ -16,6 +16,13 @@ define :gravel_bd do
        decay: 0.25,
        sustain: 0.1,
        release: 0.1
+
+  in_thread do
+    osc "/logo/big-show"
+    sleep 0.5
+    osc "/logo/big-hide"
+    sleep 0.5
+  end
 end
 
 define :garage_door do
@@ -37,17 +44,18 @@ define :car_door_close do
          start: 0.10795, finish: 0.16945
 end
 
-define :big_horn do # possibly remove?
-  with_fx :echo, phase: 2, decay: 3 do
-    sample haus_samps, "brown_horns_2", beat_stretch: 4
+define :big_horn do
+  with_fx :echo, phase: 2, decay: 3, mix: 0.25 do
+    sample haus_samps, "brown_horns_2", attack: 1, beat_stretch: 4
   end
 end
 
 define :horny_bass do |note, len|
-  use_synth :dpulse
+  use_synth :fm
   pw = 0.25
-  s = play note, attack: 0.05, attack_level: 1, decay: 0.195*len, sustain: (0.2), sustain_level: 0.75, release: 0.3*len,
-           pulse_width: pw, pulse_width_slide: 1*len,
+  s = play note, attack: 0.1*(1.0/len), attack_level: 1, decay: 0.195*len, sustain: 0.1, sustain_level: 0.75, release: 0.3*len,
+           divisor: 1.501, depth: 2,
+           #pulse_width: pw, pulse_width_slide: 1*len,
            cutoff: 115, cutoff_slide: 2
   control s, cutoff: 95
 end
@@ -60,18 +68,25 @@ define :animal_house do
   end
 end
 
-define :creaky_door do
-
-end
-
 define :keys do
   sample haus_samps, "keyring", beat_stretch: 8
 end
 
 define :haus_keys do
   sample haus_samps, "keyring", onset: tick(:os)
+  in_thread do
+    osc "/logo/little-show"
+    sleep 0.25
+    osc "/logo/little-hide"
+    sleep 0.75
+  end
 end
 
 define :windward do
+
+end
+
+#drop down to just creaky door and keys
+define :creaky_door do
 
 end
